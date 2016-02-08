594f05c745fda22762bdbb38f95bc634454af6fb
prussian <generalunrest@airmail.cc>
2016-02-08 14:44:43 -0500

    allows the bot to listen for name, the default postfix for hexchat

--- README.md
+++ README.md
@@ -2,9 +2,13 @@
 ==========
 
 simple 8ball bot.
-call it by invoking it's nick with a colon at the end
+call it by invoking it's nick with a colon or a comma at the end
 
 e.g.
 
 	you      | the8ball: Will I find a better job?
 	the8ball | You: Yes.
+
+to have the bot join another channel, you can invite it to that channel
+
+	/invite the8ball
--- app.js
+++ app.js
@@ -1,39 +1,23 @@
 #!/usr/bin/env node
-// irc client stuff
-var answers = [ 'Signs point to yes.',
-				'Yes.',
-				'Reply hazy, try again.',
-				'Without a doubt.',
-				'My sources say no.',
-				'As I see it, yes.',
-				'You may rely on it.',
-				'Concentrate and ask again.',
-				'Outlook not so good.',
-				'It is decidedly so.',
-				'Better not tell you now.',
-				'Very doubtful.',
-				'Yes - definitely.',
-				'It is certain.',
-				'Cannot predict now.',
-				'Most likely.',
-				'Ask again later.',
-				'My reply is no.',
-				'Outlook good.',
-				'Don\'t count on it.'];
+
+var answers = [ 'Signs point to yes.', 'Yes.', 'Reply hazy, try again.', 'Without a doubt.', 'My sources say no.', 'As I see it, yes.', 'You may rely on it.', 'Concentrate and ask again.', 'Outlook not so good.', 'It is decidedly so.', 'Better not tell you now.', 'Very doubtful.', 'Yes - definitely.', 'It is certain.', 'Cannot predict now.', 'Most likely.', 'Ask again later.', 'My reply is no.', 'Outlook good.', 'Don\'t count on it.'];
 
 var irc_config = require('./config');
 var irc = require('irc');
 var client = new irc.Client(irc_config.server,irc_config.nick,irc_config.options);
 
 // begin listening for messages
+// it only listens for channel messages
+// private messages will be ignored
 client.addListener('message#',function (from, to, text, message) {
-	// if message contains $NICK:
-	if (text.match(RegExp(irc_config.nick+':'))) {
+	// if message contains ${NICK}: or ${NICK},
+	if (text.match(RegExp(irc_config.nick+'(:|,)'))) {
 		client.say(to,from+": "+answers[Math.floor(Math.random()*answers.length)]);
 	}
 }); 
 
 // listen for invites
+// /invite $NICK will cause the bot to join that channel
 client.addListener('invite',function(channel, from, message) {
 	client.join(channel);
 });
